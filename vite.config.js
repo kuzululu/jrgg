ef1c2f0ae0d7f0ff520ff3a3e2d65a4fb12663c1
Jeff Ronald Gamasan <159509139+kuzululu@users.noreply.github.com>
2025-07-31 15:58:06 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,6 @@ dotenv.config()
 export default defineConfig({
   plugins: [react()],
   server:{
-    host: '0.0.0.0', // <-- Add this line to allow LAN access
-    port: Number(process.env.VITE_PORT),
     base: "/reactPortfolio"
   }
 })
