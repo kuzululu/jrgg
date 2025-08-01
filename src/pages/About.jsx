11580b0dc20f3cd078368c22ee5b833d4050f567
Jeff Ronald Gamasan <159509139+kuzululu@users.noreply.github.com>
2025-08-01 08:06:05 +0800

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,91 +1,67 @@
 import { useEffect, useRef, useState } from "react"
 import { Link } from "react-router-dom"
+// for dynamic website name
 import { Helmet } from "react-helmet-async"
-import Typed from "typed.js"
-import TypingEffect from "../shared/components/TypingEffect"
-import profile from "../assets/images/profile/slider-commapp_v3_1.jpg"
+import ModalPortfolio from "../shared/components/ModalPortfolio"
 
-const AboutPage = () => {
+import accounting from '../assets/images/portfolio/slider-accounting.jpg'
+import capacitance from '../assets/images/portfolio/slider-capacitance.jpg'
+import commapp_v3 from '../assets/images/portfolio/slider-commapp_v3.jpg'
+import corinthians from '../assets/images/portfolio/slider-corinthians.jpg'
+import ezh_web from '../assets/images/portfolio/slider-ezh_web.jpg'
+import life1corp from '../assets/images/portfolio/slider-life1corp.jpg'
+import sample from '../assets/images/portfolio/slider-sample.jpg'
+import school from '../assets/images/portfolio/slider-school.jpg'
+
+const PortfolioPage = () => {
   return (
     <>
+
+    <Helmet>
+      <title>React Webpage Portfolio | Portfolio</title>
+    </Helmet>
      
-     <Helmet>
-      <title>React Webpage Portfolio | About</title>
-     </Helmet>
+    <ModalPortfolio />
 
-     <section id="about" className="text-light">
-
+    <section id="portfolio" className="mt-4">
       <div className="container-fluid">
-       <div className="row">
-
-        <div className="col-md-3 col-lg-3 p-3 border-top border-end border-primary border-2" id="profilInfo">
-        <h1 className="text-uppercase text-center fw-bolder text-light animate__animated animate__fadeIn animate__infinite infinite animate__slow">About Me</h1>
-
-        <center><img src={profile} className="w-75 img-fluid" id="portfolio" /></center>
-        <div className="mt-3 text-light">
-            <h6><i className="fa fa-user" /> Jeff Ronald G. Gamasan</h6>
-            <h6><i className="fa fa-phone-alt" /> 09452869822</h6>
-            <h6><i className="fa fa-envelope" /> jeffgamasan@gmail.com</h6><hr/>
-
-            <h5 className="text-center text-uppercase">Freelance</h5>
-            <h6><i className="fa-solid fa-code"></i> FrontEnd Web developer(HTML, CSS3, JS, BS5) </h6>
-            <h6><i className="fa-solid fa-code"></i> ReactJS developer</h6>
-            <h6><i className="fa-solid fa-code"></i> PHP+MySQL, Laravel, CodeIgniter developer</h6><hr/>
-            
-            <h5 className="text-uppercase text-center">Current Work</h5>
-            <h6><i className="fa-solid fa-headset"></i> IT techincal Support - Government Employee</h6><hr/>
-            
-            <h5 className="text-center text-uppercase">Past Works</h5>      
-            <h6><i className="fa-solid fa-chalkboard-user"></i> IT Instructor - Private School (AZGH College Inc)</h6>      
-            <h6><i className="fa-solid fa-headset"></i> IT Technical Support - A. Zarate General Hospital</h6>      
-        </div>
+        
+        <div className="row mb-3">
+          <div className="col-md-3 col-lg-3 mb-md-0 mb-lg-0 mb-3">
+            <a href="#" data-bs-toggle="modal" data-bs-target="#modalAccounting"><img src={ accounting } className="img-fluid img-thumbnail w-100" /></a>
+          </div>
+          <div className="col-md-3 col-lg-3 mb-md-0 mb-lg-0 mb-3">
+           <a href="#" data-bs-toggle="modal" data-bs-target="#modalCapacitance"><img src={ capacitance } className="img-fluid img-thumbnail w-100" /></a>
+          </div>
+          <div className="col-md-3 col-lg-3 mb-md-0 mb-lg-0 mb-3">
+            <a href="#" data-bs-toggle="modal" data-bs-target="#modalComappt"><img src={ commapp_v3 } className="img-fluid img-thumbnail w-100" /></a>
+          </div>
+          <div className="col-md-3 col-lg-3 mb-md-0 mb-lg-0 mb-3">
+            <a href="#" data-bs-toggle="modal" data-bs-target="#modalCorinthians"><img src={ corinthians } className="img-fluid img-thumbnail w-100" /></a>
+          </div>
         </div>
 
-        <div className="col-md-9 col-lg-9 pt-md-4 pt-lg-4 pt-2">
-            
-          <h2 className="position-absolute"><TypingEffect /> {/* 👈 Insert here */}</h2>
-          
-          <p className="about-me pt-5">
-            Hi, I'm Jeff Ronald Gamasan from the Philippines. I was an IT Instructor from 2015 to 2020, where I had the privilege of teaching various areas in Information Technology — particularly focusing on frontend web development. Currently, I work as an IT Technician in a government office, handling technical support and system maintenance.</p>
-
-           <p className="about-me">Although I have years of experience, I humbly consider myself a lifelong learner. As a web developer, I treat myself as a newbie every day — always open to learning new things and improving my craft. I believe that staying grounded and constantly curious is key to growing in this ever-evolving field.
-
-            I’m now looking to take on freelance opportunities, especially in frontend web development, to further enhance my skills and work on real-world projects. My current skill set includes:</p>
-
-            <code className="about-me-list text-light fw-bolder">
-              <ul>
-              <li>HTML</li>
-              <li>CSS3</li>
-              <li>Bootstrap</li>
-              <li>JavasCript, JQuery</li>
-              <li>React JS</li>
-              <li>PHP+MySQL</li>
-              <li>
-                PHP Frameworks
-                <ul>
-                   <li>Laravel</li>
-                   <li>CodeIginiter</li>
-                </ul>
-              </li>
-            </ul>
-
-            </code>
-            <p className="about-me">If you’re looking for someone to help you build or improve your website, I’d be happy to collaborate. I believe in earning trust through quality work — so you can test my skills first, and if you're not satisfied, you are not obliged to pay.
-
-            Feel free to reach out to me via email: jeffgamasan@gmail.com
-
-            Looking forward to working with you!
-          </p>
-
+        <div className="row mb-5 mt-5">
+          <div className="col-md-3 col-lg-3 mb-md-0 mb-lg-0 mb-3">
+           <a href="#" data-bs-toggle="modal" data-bs-target="#modalEZHweb"><img src={ ezh_web } className="img-fluid img-thumbnail w-100" /></a>
+          </div>
+          <div className="col-md-3 col-lg-3 mb-md-0 mb-lg-0 mb-3">
+            <a data-bs-toggle="modal" data-bs-target="#modalLife1" href="#"><img src={ life1corp } className="img-fluid img-thumbnail w-100" /></a>
+          </div>
+          <div className="col-md-3 col-lg-3 mb-md-0 mb-lg-0 mb-3">
+            <a data-bs-toggle="modal" data-bs-target="#modalSample" href="#"><img src={ sample } className="img-fluid img-thumbnail w-100" /></a>
+          </div>
+          <div className="col-md-3 col-lg-3">
+            <a data-bs-toggle="modal" data-bs-target="#modalSchool" href="#"><img src={ school } className="img-fluid img-thumbnail w-100" /></a>
+          </div>
         </div>
 
-       </div>
       </div>
-
     </section>
-     
+
     </>
+
   )
 }
 
-export default AboutPage
+export default PortfolioPage
