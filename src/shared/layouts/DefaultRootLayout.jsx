d37dec3d3499e1c711f339297747254c30dc14ae
Jeff Ronald Gamasan <159509139+kuzululu@users.noreply.github.com>
2025-08-09 12:52:58 +0800

    Update DefaultRootLayout.jsx

--- src/shared/layouts/DefaultRootLayout.jsx
+++ src/shared/layouts/DefaultRootLayout.jsx
@@ -6,14 +6,9 @@ import { Outlet } from 'react-router-dom'
 const DefaultLayout = () =>{
     return(
      <>
-        {/* for footer is always at the bottom */}
-        <div className="d-flex flex-column min-vh-100">
-         <Navbar />
-         <main className="flex-fill">
-          <Outlet />
-         </main>
-         <Footer />
-        </div>
+       <Navbar />
+       <Outlet />
+       <Footer />
      </> 
     )
 }
