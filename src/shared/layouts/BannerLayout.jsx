0f80797b953086add7ae70a2806a8cf9222e3d90
Jeff Ronald Gamasan <159509139+kuzululu@users.noreply.github.com>
2025-08-09 12:52:34 +0800

    Update BannerLayout.jsx

--- src/shared/layouts/BannerLayout.jsx
+++ src/shared/layouts/BannerLayout.jsx
@@ -7,20 +7,15 @@ import { Link, Outlet } from 'react-router-dom'
 const BannerLayout = () =>{
  return(
     <>
-    {/* for footer is always at the bottom */}
-  <div className="d-flex flex-column min-vh-100">
-    <div className='d-md-none d-lg-none d-block sticky-top'>
+     <div className='d-md-none d-lg-none d-block sticky-top'>
       <Navbar />
       </div> 
      <Banner />
-   <main className="flex-fill">
     <div className='d-md-block d-lg-block d-none sticky-top'>
       <Navbar />
     </div>
      <Outlet />
-   </main>
-   <Footer />
-  </div>
+     <Footer />
     </>
  )
 }
